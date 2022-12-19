5621022e2e1e24f08eb71b6b2532cadabc5a2ac5
lateigne9 <fabien.terres@gmail.com>
2022-12-19 12:03:12 -0500

    implemantation classes Paquet.java, Joueur.java et Jeu.java.

--- src/UNO/Jeu.java
+++ src/UNO/Jeu.java
@@ -4,15 +4,16 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Jeu {
-    private int nombreDeJoueur = 2;
-    ArrayList<Joueur> joueurs = new ArrayList<>();
-    Paquet paquet = new Paquet();
+    private int nombreDeJoueur;
+    private ArrayList<Joueur> joueurs = new ArrayList<>();
+    private Paquet paquet = new Paquet();
 
     private static Carte table;
 
     private final static int nombreDeCarteDistribue = 7;
 
-    public Jeu() {
+    public Jeu(int nombreDeJoueurs) {
+        this.nombreDeJoueur=nombreDeJoueurs;
         boolean finPartie = false;/*booleen qui determine si la partie est fini ou pas*/
         initialistationJoueur();/*rentre les noms des joueurs dans le jeu et leur donner une main*/
         //TODO Mettre une carte sur la table
@@ -21,8 +22,8 @@ public class Jeu {
         while (!finPartie) {
             for (Joueur joueur : joueurs) {
                 tour(joueur);
-                finPartie=finPartie(joueur);
-                if (finPartie){
+                finPartie = finPartie(joueur);
+                if (finPartie) {
                     break;
                 }
             }
@@ -91,13 +92,52 @@ public class Jeu {
         distribution();
     }
 
+    /**Méthode qui simule le tour d'un joueur
+     * @param joueur joueur qui joue
+     */
     private void tour(Joueur joueur) {
+        System.out.println(descriptionTour(joueur));
         //TODO voir si le joueur peut jouer
-
+        int TMJoueurDebut=joueur.getMain().size();
+        for (int i = 0; i < TMJoueurDebut; i++) {
+            if (joueur.getMain().get(i).estjouable(table)) {/*parcours la main du joueur à la recherche de la première carte jouable et la joue*/
+                joueCarte(joueur, i);
+                break;
+            }
+        }
+        if (TMJoueurDebut==joueur.getMain().size()){
+            pioche(joueur);
+        }
     }
 
-    private void pioche(Joueur joueur){
+    /**Méthode qui décrit la table ainsi que la main du joueur qui joue ce tour
+     * @param joueur joueur qui joue
+     * @return chaine de charactères formatée
+     */
+    private String descriptionTour(Joueur joueur){
+        return "\nSur la table il y a \n"+
+                table+
+                "\nAu tour de "+joueur.getNom()+" de jouer avec la main: "+joueur.getMain()+'\n';
+    }
+
+    /**
+     * Méthode qui joue une carte de la main du joueur et l'enlève de sa main
+     * @param joueur     joueur qui joue
+     * @param placeCarte place de la carte à jouer dans la main du joueur
+     */
+    private void joueCarte(Joueur joueur, int placeCarte) {
+        table = joueur.getMain().get(placeCarte);
+        System.out.println(joueur.getNom() + " a joué " + table);
+        joueur.getMain().remove(placeCarte);
+    }
+
+    /**Methode qui fait piocher le joueur
+     * @param joueur joueur qui pioche
+     */
+    private void pioche(Joueur joueur) {
         joueur.getMain().add(paquet.donneCarte());
+        int dernierecarte=joueur.getMain().size();
+        System.out.println(joueur.getNom()+"a pioché "+joueur.getMain().get(dernierecarte-1));
     }
 
     @Override
--- src/UNO/TestJeu.java
+++ src/UNO/TestJeu.java
@@ -2,7 +2,6 @@ package UNO;
 
 public class TestJeu {
     public static void main(String[] args) {
-        Jeu test=new Jeu();
-        System.out.println(test);
+        new Jeu(2);
     }
 }
